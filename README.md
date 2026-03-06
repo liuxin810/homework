f1a6697f382e69d50a92e9fa94d674bb04dd9dab
liuxin810 <18700381727@163.com>
2026-03-06 14:26:45 +0800

    增强功能：为 HelloWorld 增加 input 输入

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # homework
+
 my homework
 #这是我的github仓库
 这个仓库用于完成我的git作业
+
+\## 版本更新记录
+
+\- v1.0：初始版本，输出 Hello World
+
+\- v2.0：增加 input 输入功能，可以显示用户输入的名字
+
--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+
 int main() {
-    printf("Hello World!\n");
+    char name[50];  // 定义一个字符数组，用于存储用户输入的名字
+    
+    printf("请输入你的名字：");
+    scanf("%s", name);  // 获取用户输入（注意：%s 不能输入空格）
+    
+    printf("你好，%s！欢迎学习 C 语言。\n", name);
+    printf("程序运行完毕。\n");
+    
     return 0;
 }
